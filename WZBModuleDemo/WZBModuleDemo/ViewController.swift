a877389d55a227aa3ad993cf055dd465a7511d3a
zhibin wang <zhibin.wang@net263.com>
2020-08-17 13:52:45 +0800

    project init

---
+++ WZBModuleDemo/WZBModuleDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WZBModuleDemo
+//
+//  Created by zhibin wang on 2020/8/17.
+//  Copyright © 2020 iTalkBB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
